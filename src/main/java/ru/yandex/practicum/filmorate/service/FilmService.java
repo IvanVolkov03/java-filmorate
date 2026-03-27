fb4423b8e02382bf8422d8892f96cafb9f88fac0
Ivan Volkov <ivolkov03@mail.ru>
2026-03-27 19:51:56 +0300

    Усовершенствовать фильмотеку

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,20 +1,20 @@
 package ru.yandex.practicum.filmorate.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class FilmService {
     private final FilmStorage filmStorage;
-
-    @Autowired
-    public FilmService(FilmStorage filmStorage) {
-        this.filmStorage = filmStorage;
-    }
+    private final UserStorage userStorage;
 
     public Film create(Film film) {
         return filmStorage.create(film);
@@ -33,14 +33,19 @@ public class FilmService {
     }
 
     public void addLike(int filmId, int userId) {
+        log.info("Добавление лайка: filmId={}, userId={}", filmId, userId);
+        userStorage.findById(userId);
         filmStorage.addLike(filmId, userId);
     }
 
     public void removeLike(int filmId, int userId) {
+        log.info("Удаление лайка: filmId={}, userId={}", filmId, userId);
+        userStorage.findById(userId);
         filmStorage.removeLike(filmId, userId);
     }
 
     public List<Film> getPopularFilms(int count) {
+        log.info("Получение популярных фильмов, count={}", count);
         return filmStorage.getPopularFilms(count);
     }
 }
